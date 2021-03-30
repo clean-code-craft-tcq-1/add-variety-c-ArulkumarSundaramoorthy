061ab5a08eaeb3a8cff40c4dd1ec630b4531b0e1
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-03-30 15:03:58 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,5 +1,13 @@
+/* *************************************************************************
+* File Name   :	sensor-validate.h
+* Description : Validate SOC & Current Readings
+* Functions	  : - 
+* ************************************************************************* */
+#ifndef TYPEWISE_ALERT_H
+#define TYPEWISE_ALERT_H
 #pragma once
 
+/* ***************************** Typedef ***************************** */
 typedef enum {
   PASSIVE_COOLING,
   HI_ACTIVE_COOLING,
@@ -12,8 +20,10 @@ typedef enum {
   TOO_HIGH
 } BreachType;
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+typedef struct {
+	double lowerLimit;
+	double upperLimit;
+}BatteryBreachLimit;
 
 typedef enum {
   TO_CONTROLLER,
@@ -25,8 +35,11 @@ typedef struct {
   char brand[48];
 } BatteryCharacter;
 
+/* ***************************** Prototypes ***************************** */
 void checkAndAlert(
   AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
-
 void sendToController(BreachType breachType);
 void sendToEmail(BreachType breachType);
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+
+#endif
