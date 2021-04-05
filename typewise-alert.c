e8b1dd03302afe20c1730784fd6b85d5e06bd358
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-04-05 17:33:43 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -1,10 +1,11 @@
 /* *************************************************************************
 * File Name   :	typewise-alert.c
 * Description : Battery Limit check based on Cooling Type
-* Functions	  : 1. inferBreach
-*				2. checkAndAlert
-*				3. sendToController
-*				4. sendToEmail
+* Functions  : 1. inferBreach
+*	       2. checkAndAlert
+*              3. sendToController
+*	       4. sendToEmail
+*	       5. sendToConsole
 * ************************************************************************* */
 
 /* ***************************** Header Files ***************************** */
@@ -13,14 +14,15 @@
 
 /* *****************************  Variables  ****************************** */
 BatteryBreachLimit BatteryCoolTypeLimit[3] = {{0,35},{0,45},{0,40}};
+SendStatus(*SendAlertType[])(BreachType) = { sendToController , sendToEmail, sendToConsole};
 
 /* *************************************************************************
 * Function Name : inferBreach
 * Description   : Check the breach limit
-* Arguments	    : 1. value - values for compare
-*				  2. lowerLimit - another values for compare
-*				  3. upperLimit - Maximum acceptable changes
-* Returns		: BreachType(NORMAL / TOO_LOW / TOO_HIGH)
+* Arguments	: 1. value - values for compare
+*		  2. lowerLimit - another values for compare
+*		  3. upperLimit - Maximum acceptable changes
+* Returns	: BreachType(NORMAL / TOO_LOW / TOO_HIGH)
 * ************************************************************************* */
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   if(value < lowerLimit) {
@@ -35,45 +37,42 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
 /* *************************************************************************
 * Function Name : checkAndAlert
 * Description   : Check the breach limit & send a alert
-* Arguments	    : 1. alertTarget - Alert type
-*				  2. batteryChar - Cooling Type
-*				  3. temperatureInC - Temperature value
-* Returns		: -
+* Arguments	: 1. alertTarget - Alert type
+*		  2. batteryChar - Cooling Type
+*		  3. temperatureInC - Temperature value
+* Returns	: Send Fail or Pass
 * ************************************************************************* */
 void checkAndAlert(
     AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+	
+	SendStatus ReturnStatus = SEND_FAIL;
 
 	BreachType breachType = inferBreach(temperatureInC, BatteryCoolTypeLimit[batteryChar.coolingType].lowerLimit, 
 								BatteryCoolTypeLimit[batteryChar.coolingType].upperLimit);
-
-  switch(alertTarget) {
-    case TO_CONTROLLER:
-      sendToController(breachType);
-      break;
-    case TO_EMAIL:
-      sendToEmail(breachType);
-      break;
-  }
+	ReturnStatus = SendAlertType[alertTarget](breachType);
+	
+	return ReturnStatus;
 }
 
 /* *************************************************************************
 * Function Name : sendToController
 * Description   : send a alert to Controller
-* Arguments	    : 1. BreachType(NORMAL / TOO_LOW / TOO_HIGH)
-* Returns		: -
+* Arguments	: 1. BreachType(NORMAL / TOO_LOW / TOO_HIGH)
+* Returns	: Send Fail or Pass
 * ************************************************************************* */
-void sendToController(BreachType breachType) {
+ SendStatus sendToController(BreachType breachType) {
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
+  return SEND_PASS;
 }
 
 /* *************************************************************************
 * Function Name : sendToEmail
 * Description   : send a alert via Email
-* Arguments	    : 1. BreachType(NORMAL / TOO_LOW / TOO_HIGH)
-* Returns		: -
+* Arguments	: 1. BreachType(NORMAL / TOO_LOW / TOO_HIGH)
+* Returns	: Send Fail or Pass
 * ************************************************************************* */
-void sendToEmail(BreachType breachType) {
+SendStatus sendToEmail(BreachType breachType) {
   const char* recepient = "a.b@c.com";
   printf("To: %s\n", recepient);
 	
@@ -85,4 +84,17 @@ void sendToEmail(BreachType breachType) {
       printf("Hi, the temperature is too high\n");
       break;
   }
+	
+ return SEND_PASS;
+}
+
+/* *************************************************************************
+* Function Name : sendToConsole
+* Description   : send a alert to Controller
+* Arguments	: 1. BreachType(NORMAL / TOO_LOW / TOO_HIGH)
+* Returns	: Send Fail or Pass
+* ************************************************************************* */
+ SendStatus sendToConsole(BreachType breachType) {
+  printf("Console : %x\n", breachType);
+  return SEND_PASS;
 }
