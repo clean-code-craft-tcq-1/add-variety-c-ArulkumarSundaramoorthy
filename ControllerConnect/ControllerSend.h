d60920186c171589c564bac182bf15a794c1dd4f
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-04-17 15:57:48 +0530

    Update ControllerSend.h

--- ControllerConnect/ControllerSend.h
+++ ControllerConnect/ControllerSend.h
@@ -6,9 +6,6 @@
 #ifndef CONTROLLER_SEND_H
 #define CONTROLLER_SEND_H
 
-/* ***************************** Header Files ***************************** */
-#include "typewise-alert.h"
-
 /* ***************************** Prototypes ***************************** */
 SendStatus sendToController(BreachType breachType);
 
