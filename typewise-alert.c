30d884da0223ec566021322d04d19b64baf0ee03
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-04-17 15:37:49 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -3,9 +3,8 @@
 * Description : Battery Limit check based on Cooling Type
 * Functions  : 1. inferBreach
 *	       2. checkAndAlert
-*              3. sendToController
-*	       4. sendToEmail
-*	       5. sendToConsole
+*	       3. sendToEmail
+*	       4. sendToConsole
 * ************************************************************************* */
 
 /* ***************************** Header Files ***************************** */
@@ -53,20 +52,6 @@ SendStatus checkAndAlert(
 	ReturnStatus = SendAlertType[alertTarget](breachType);
 	
 	return ReturnStatus;
-}
-
-
-
-/* *************************************************************************
-* Function Name : sendToController
-* Description   : send a alert to Controller
-* Arguments	: 1. BreachType(NORMAL / TOO_LOW / TOO_HIGH)
-* Returns	: Send Fail or Pass
-* ************************************************************************* */
- SendStatus sendToController(BreachType breachType) {
-  const unsigned short header = 0xfeed;
-  printf("%x : %x\n", header, breachType);
-  return CONTROLLER_SEND_PASS;
 }
 
 /* *************************************************************************
