c15258d7f2aa7819d18b1564df4e3f80fd65a432
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-04-17 16:30:27 +0530

    Update ControllerSend.c

--- ControllerConnect/ControllerSend.c
+++ ControllerConnect/ControllerSend.c
@@ -16,6 +16,5 @@
 * ************************************************************************* */
  SendStatus sendToController(BreachType breachType) {
    //Send to controller
-  BreachType breachType1 = breachType;
-  return CONTROLLER_SEND_PASS;
+  return breachType;
 }
