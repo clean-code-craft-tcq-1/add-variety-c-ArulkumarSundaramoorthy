fee3f8bfb54d74d73ed5b126c42fe96455a471ad
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-04-17 15:18:28 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -13,26 +13,26 @@ TEST_CASE("infers the breach according to limits HIGH") {
 
 TEST_CASE("Test the Send operation - Controller") {
   BatteryCharacter batterychar = {PASSIVE_COOLING, "ABC"};
-  REQUIRE( checkAndAlert(TO_CONTROLLER, batterychar, 25)== SEND_PASS);
+  REQUIRE( checkAndAlert(TO_CONTROLLER, batterychar, 25)== CONTROLLER_SEND_PASS);
 }
 
 TEST_CASE("Test the Send operation - Email") {
   BatteryCharacter batterychar = {HI_ACTIVE_COOLING, "ABC"};
-  REQUIRE( checkAndAlert(TO_EMAIL, batterychar, 40)== SEND_PASS);
+  REQUIRE( checkAndAlert(TO_EMAIL, batterychar, 40)== EMAIL_SEND_PASS);
 }
 
 
 TEST_CASE("Test the Send operation - Email / High") {
   BatteryCharacter batterychar = {HI_ACTIVE_COOLING, "ABC"};
-  REQUIRE( checkAndAlert(TO_EMAIL, batterychar, 50)== SEND_PASS);
+  REQUIRE( checkAndAlert(TO_EMAIL, batterychar, 50)== EMAIL_SEND_PASS);
 }
 
 TEST_CASE("Test the Send operation - Email / Low") {
   BatteryCharacter batterychar = {HI_ACTIVE_COOLING, "ABC"};
-  REQUIRE( checkAndAlert(TO_EMAIL, batterychar, -1)== SEND_PASS);
+  REQUIRE( checkAndAlert(TO_EMAIL, batterychar, -1)== EMAIL_SEND_PASS);
 }
 
 TEST_CASE("Test the Send operation - Console") {
   BatteryCharacter batterychar = {MED_ACTIVE_COOLING, "ABC"};
-  REQUIRE( checkAndAlert(TO_CONSOLE, batterychar, 30)== SEND_PASS);
+  REQUIRE( checkAndAlert(TO_CONSOLE, batterychar, 30)== CONSOLE_SEND_PASS);
 }
