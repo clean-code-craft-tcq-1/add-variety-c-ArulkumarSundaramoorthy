3336d2406fba84f912e11f1b7a1828647247300f
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-04-17 15:37:37 +0530

    Create ControllerSend.h

---
+++ ControllerConnect/ControllerSend.h
@@ -0,0 +1,12 @@
+/* *************************************************************************
+* File Name   :	sensor-validate.h
+* Description : Validate SOC & Current Readings
+* Functions	  : - 
+* ************************************************************************* */
+#ifndef CONTROLLER_SEND_H
+#define CONTROLLER_SEND_H
+
+/* ***************************** Prototypes ***************************** */
+SendStatus sendToController(BreachType breachType);
+
+#endif
